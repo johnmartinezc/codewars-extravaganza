c0ef3db36f163f1a72bc3c4de8bc57cbcfb7eaa5
johnmartinezc <john.martinez@codeimmersives.com>
2022-10-04 10:42:02 -0400

    -a

--- sum_of_numbers.js
+++ sum_of_numbers.js
@@ -12,14 +12,3 @@ function getSum(a, b) {
 
 }
 console.log(getSum(4, 3))
-
-// function GetSum(a, b) {
-//     if (a == b) {
-//         return a
-//     } else if (a < b) {
-//         return a + GetSum(a + 1, b)
-//     } else {
-//         return a + GetSum(a - 1, b)
-//     };
-// }
-// console.log(GetSum(2, 2));
